e5dcc98adf902a4f9c2bf3a633bee0c3ec781fae
WosA <anna.wos@fujitsu.com>
2023-02-12 16:02:40 +0100

    find letter first commit

--- src/main/java/FindLetter.java
+++ src/main/java/FindLetter.java
@@ -4,16 +4,14 @@ class FindLetter {
     public ArrayList<Integer> findLetter(String letter, String word) {
         int i = 0;
         int firstIndex = word.indexOf(letter, i);
-//            System.out.println("First occurrence of char 's'"
-//                    + " is found at : "
-//                    + firstIndex);
         ArrayList<Integer> listt = new ArrayList<Integer>();
         if (firstIndex == -1) {
             listt.add(firstIndex);
             return listt;
         } else {
             for (i = 1; i <= word.length(); i++) {
-                listt.add(word.indexOf(letter, i));
+                while( word.indexOf(letter, i) >= 0)
+                    listt.add(word.indexOf(letter, i));
             }
             return listt;
         }
